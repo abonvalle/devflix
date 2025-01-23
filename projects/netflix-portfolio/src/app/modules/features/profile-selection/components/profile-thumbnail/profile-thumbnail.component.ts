2338406025b036de7b1734b1421e3b2b0058e520
abonvalle <abonvalle@proton.me>
2025-01-23 12:45:16 +0100

    Refactor application structure to use standalone components and remove the AppModule for improved modularity

--- projects/netflix-portfolio/src/app/modules/features/profile-selection/components/profile-thumbnail/profile-thumbnail.component.ts
+++ projects/netflix-portfolio/src/app/modules/features/profile-selection/components/profile-thumbnail/profile-thumbnail.component.ts
@@ -3,10 +3,11 @@ import { Component, Input } from '@angular/core';
 import { Profile } from '@models/profile.interface';
 
 @Component({
-    selector: 'app-profile-thumbnail',
-    templateUrl: './profile-thumbnail.component.html',
-    styleUrls: ['./profile-thumbnail.component.scss'],
-    imports: [CommonModule]
+  selector: 'app-profile-thumbnail',
+  templateUrl: './profile-thumbnail.component.html',
+  styleUrls: ['./profile-thumbnail.component.scss'],
+  imports: [CommonModule],
+  standalone: true
 })
 export class ProfileThumbnailComponent {
   @Input({ required: true }) profile!: Profile;
--- projects/netflix-portfolio/src/app/modules/features/profile/profile-loading/profile-loading.component.ts
+++ projects/netflix-portfolio/src/app/modules/features/profile/profile-loading/profile-loading.component.ts
@@ -1,12 +1,11 @@
 import { Component, Input } from '@angular/core';
-import { ProfileThumbnailComponent } from '@features/profile-selection/components/profile-thumbnail/profile-thumbnail.component';
 import { Profile } from '@models/profile.interface';
 
 @Component({
-    selector: 'app-profile-loading',
-    imports: [ProfileThumbnailComponent],
-    templateUrl: './profile-loading.component.html',
-    styleUrl: './profile-loading.component.scss'
+  selector: 'app-profile-loading',
+  templateUrl: './profile-loading.component.html',
+  styleUrl: './profile-loading.component.scss',
+  standalone: true
 })
 export class ProfileLoadingComponent {
   @Input({ required: true }) profile!: Profile;
